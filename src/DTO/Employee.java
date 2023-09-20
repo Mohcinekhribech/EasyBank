e24d71d62e04869f4b0858762d870fbc057044ad
Mohcinekhribech <mohcinekhribche123@gmail.com>
2023-09-20 20:19:20 +0200

    Diagramme

---
+++ src/DTO/Employee.java
@@ -0,0 +1,8 @@
+package DTO;
+
+public class Employee {
+    private String registrationNumber ;
+    private Date recruitmentDate;
+
+
+}
---
+++ src/DTO/User.java
@@ -0,0 +1,49 @@
+package DTO;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String phoneNumber;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
