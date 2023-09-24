36706c31d71c99052f7509ca56af8863da098b50
Mohcinekhribech <mohcinekhribche123@gmail.com>
2023-09-24 19:23:59 +0200

    EAS-6 Add Employee

--- src/DAO/EmployeDao.java
+++ src/DAO/EmployeDao.java
@@ -6,24 +6,27 @@ import Interfaces.EmployeInterface;
 
 import java.sql.*;
 import java.util.List;
+import java.util.Optional;
 
 public class EmployeDao implements EmployeInterface {
     Connection connection = Database.ConnectToDb();
     @Override
-    public Employee add(Employee employee) throws SQLException {
+    public Optional<Employee> add(Optional<Employee> employee) throws SQLException {
+        if(employee.isEmpty())
+            return null;
         try {
             this.connection.setAutoCommit(false);
             PreparedStatement statement = this.connection.prepareStatement("INSERT INTO person (firstName, lastName, dateofbirth, phonenumber) VALUES (?, ?, ?, ?) ;");
             PreparedStatement statement1 = this.connection.prepareStatement("INSERT INTO employe (id, registrationNumber, recrutmentDate, email) VALUES ( (SELECT id FROM person WHERE firstName = ? AND lastName = ?), ?, ?, ?);");
-            statement.setString(1,employee.getFirstName());
-            statement.setString(2,employee.getLastName());
-            statement.setDate(3, employee.getDateOfBirth());
-            statement.setString(4,employee.getPhoneNumber());
-            statement1.setString(1,employee.getFirstName());
-            statement1.setString(2,employee.getLastName());
-            statement1.setString(3,employee.getRegistrationNumber());
-            statement1.setDate(4, employee.getRecruitmentDate());
-            statement1.setString(5,employee.getEmail());
+            statement.setString(1,employee.get().getFirstName());
+            statement.setString(2,employee.get().getLastName());
+            statement.setDate(3, employee.get().getDateOfBirth());
+            statement.setString(4,employee.get().getPhoneNumber());
+            statement1.setString(1,employee.get().getFirstName());
+            statement1.setString(2,employee.get().getLastName());
+            statement1.setString(3,employee.get().getRegistrationNumber());
+            statement1.setDate(4, employee.get().getRecruitmentDate());
+            statement1.setString(5,employee.get().getEmail());
             if(statement.execute() && statement1.execute())
             {
                 this.connection.commit();
@@ -36,25 +39,27 @@ public class EmployeDao implements EmployeInterface {
         }finally {
             this.connection.setAutoCommit(true);
         }
-        return employee;
+        return null;
     }
+
     @Override
     public int delete(String registrationNumber) {
         return 0;
     }
 
     @Override
-    public Employee update(Employee employee, String registrationNumber) {
-        return null;
+    public Optional<Employee> update(Employee employee, String registrationNumber) {
+        return Optional.empty();
     }
 
     @Override
-    public Employee SearchByRegistrationNumber(String registratonNumber) {
-        return null;
+    public Optional<Employee> SearchByRegistrationNumber(String registratonNumber) {
+        return Optional.empty();
     }
 
     @Override
-    public List<Employee> Search(Employee employee) {
+    public List<Optional<Employee>> Search(Employee employee) {
         return null;
     }
+
 }
