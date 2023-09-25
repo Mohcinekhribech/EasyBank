bf694e8a0cf88131cabc19f9b15260f3a4973377
Mohcinekhribech <mohcinekhribche123@gmail.com>
2023-09-25 20:07:46 +0200

    EAS-19 delete account

--- src/DAO/AccountDao.java
+++ src/DAO/AccountDao.java
@@ -3,10 +3,60 @@ package DAO;
 import DTO.Account;
 import DTO.Client;
 import DTO.Enum.Status;
+import Helpers.Database;
 import Interfaces.AccountInterface;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
-public abstract class AccountDao {
+public class AccountDao implements AccountInterface<Account>{
+    Connection connection = Database.ConnectToDb();
+    @Override
+    public Optional<Account> add(Optional<Account> account){return Optional.empty();} ;
+
+    @Override
+    public int delete(String accNum) {
+        try{
+            PreparedStatement statement = this.connection.prepareStatement("delete from account where accountNumber = ?");
+            statement.setString(1,accNum);
+            return statement.executeUpdate();
+        }catch(Exception e)
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Optional<Account> update(Optional<Account> account) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Optional<Account>> showByCreationDate(Date creationDate) {
+        return null;
+    }
+
+    @Override
+    public List<Optional<Account>> showByStatus(Status status) {
+        return null;
+    }
+
+    @Override
+    public List<Optional<Account>> show() {
+        return null;
+    }
+
+    @Override
+    public boolean changeState(String accNum) {
+        return false;
+    }
+
+    @Override
+    public List<Account> searchByClient(Client client) {
+        return null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
-import DAO.ClientDao;
-import DAO.CurrentAccountDao;
-import DAO.EmployeDao;
-import DAO.SavingAccountDao;
-import DTO.Client;
-import DTO.CurrentAccount;
-import DTO.Employee;
+import DAO.*;
+import DTO.*;
 import DTO.Enum.Status;
-import DTO.SavingAccount;
 import Helpers.Database;
 
 import java.sql.SQLException;
@@ -19,16 +13,18 @@ public class Main {
 
         ClientDao clientDao = new ClientDao();
         Client client = new Client();
-        CurrentAccountDao currentAccountDao = new CurrentAccountDao();
-        CurrentAccount currentAccount = new CurrentAccount();
-        currentAccount.setAccountNumber("1234566");
-        currentAccount.setBalance(2000);
-        currentAccount.setCreationDate(LocalDate.parse("2020-04-20"));
-        currentAccount.setMaxPrice(20000);
-        client.setCode("AZERTY");
-        currentAccount.setClient(client);
-        currentAccount.setStatus(Status.active);
-        currentAccountDao.add(Optional.of(currentAccount));
+        AccountDao accountDao = new AccountDao();
+        System.out.println(accountDao.delete("1234567"));
+//        CurrentAccountDao currentAccountDao = new CurrentAccountDao();
+//        CurrentAccount currentAccount = new CurrentAccount();
+//        currentAccount.setAccountNumber("1234566");
+//        currentAccount.setBalance(2000);
+//        currentAccount.setCreationDate(LocalDate.parse("2020-04-20"));
+//        currentAccount.setMaxPrice(20000);
+//        client.setCode("AZERTY");
+//        currentAccount.setClient(client);
+//        currentAccount.setStatus(Status.active);
+//        currentAccountDao.add(Optional.of(currentAccount));
 //        for(String keys: clientDao.searchByCode("AZERTY").keySet()){
 //            System.out.println(keys+ " : "  +clientDao.searchByCode("AZERTY").get(keys));
 //        }
